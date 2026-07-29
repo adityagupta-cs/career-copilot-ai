840f1b0fc07003092145d702e171bf38d0d4ea17
Aditya Gupta <ag796653@gmail.com>
2026-07-29 20:31:29 +0530

    Built responsive navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="min-h-screen bg-slate-900 text-white flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold text-blue-400">
-        CareerCopilot AI 🚀
-      </h1>
-
-      <p className="mt-4 text-lg text-gray-300">
-        Your AI-powered companion for placements and career growth.
-      </p>
-
-      <button className="mt-8 rounded-lg bg-blue-600 px-6 py-3 text-white hover:bg-blue-700 transition">
-        Get Started
-      </button>
+    <div className="min-h-screen bg-slate-950 text-white">
+      <Navbar />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-function Navbar(){
-    return(
-        <div>
-            <h1>Copilot</h1>
-        </div>
-    )
+import { BriefcaseBusiness } from "lucide-react";
+
+function Navbar() {
+  return (
+    <nav className="w-full flex justify-between items-center px-8 py-6">
+      <div className="flex items-center gap-2">
+        <BriefcaseBusiness className="text-blue-500" size={32} />
+        <h1 className="text-2xl font-bold text-white">
+          CareerCopilot AI
+        </h1>
+      </div>
+
+      <ul className=" flex gap-8 text-gray-300 font-medium">
+        <li className="hover:text-blue-400 transition duration-300 cursor-pointer">Home</li>
+        <li className="hover:text-blue-400 transition duration-300 cursor-pointer">Features</li>
+        <li className="hover:text-blue-400 transition duration-300 cursor-pointer">Pricing</li>
+        <li className="hover:text-blue-400 transition duration-300 cursor-pointer">Contact</li>
+      </ul>
+
+      <button className="bg-blue-600 hover:bg-blue-700 transition duration-300 hover:bg-blue-700 px-5 py-2 rounded-lg transition">
+        Login
+      </button>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
